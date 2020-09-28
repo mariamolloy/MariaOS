d01cadcc0cd0416d31f2ee423ab7102106d4daf4
Maria <mariamango3@gmail.com>
2020-09-28 00:10:19 -0400

    finished mem accessor read

--- source/host/memory.ts
+++ source/host/memory.ts
@@ -8,11 +8,6 @@ module TSOS {
       }
     }
 
-    //to do initialize mem
-    public init(): void{
-
-    }
-
     //returns a memory address
     public readMem(addy: number): String {
       //to do add to check its above 0
@@ -24,16 +19,8 @@ module TSOS {
     }
 
     //adds a byte to Memory
-    //to do: check that its valid input only rlly need line 27 in this
-    //move byte check to MA
-    //for memory accessor u can handle an array of bytes but here just one byte at a time
     public writeMem(addy: number, byte: string): void {
-      if (byte.length == 2){
-        this.mem[addy] = byte;
-      } else {
-        _StdOut.putText("Not a byte smh");
-      }
-
+      this.mem[addy] = byte;
     }
 
   }
--- source/host/memoryaccessor.ts
+++ source/host/memoryaccessor.ts
@@ -1,6 +1,7 @@
 module TSOS {
   export class MemoryAccessor {
     constructor(){}
+    
     //request checks what block to write mem in
     //to do for proj 3
     public requestMem(){
@@ -34,7 +35,19 @@ module TSOS {
   }
 
   //to do read
-  //be able to read multiple
+  //be able to read mutiple bytes w start end and PCB
+  //but PCB is for proj 3
+  //start = the memory address where we wanna start reading bytes and end is where we wanna end
+    public read(start: number, end: number, maPcb: PCB): string {
+      var amtToRead = end - start;
+      if (amtToRead == 1){
+        _Memory.readMem(start);
+      } else {
+        for (var i = 0; i < amtToRead; i++){
+          _Memory.readMem(start + i);
+        }
+      }
+    }
 
     //to do for iProj 3
     mapAddress(): void{
